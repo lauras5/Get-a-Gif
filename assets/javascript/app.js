7c0f53b9b18fc06f0c750523f02cccc9e17a22fd
Laura Salcido <idlaurasalcido@gmail.com>
2018-02-19 18:28:35 -0800

    got gifs on page, need to edit submit button val.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,18 +10,37 @@ $(document).ready(function(){
         "Kids",
     
     ]
-    var input = prompt("What would you like to search for?")
-        // $("#input").html(input)
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=y5KIXr4zxOUPpyUsmrtlZQejlJGkK563&q=&limit=25&offset=0&rating=G&lang=en"
+
+    // var input = prompt("What would you like to search for?")
+    var input;
     var APIKey = "y5KIXr4zxOUPpyUsmrtlZQejlJGkK563"
+        // $("#input").html(input)
+    var queryURL = "https://api.giphy.com/v1/stickers/search?api_key=" + APIKey + "&q=" + input + "&limit=20&offset=0&rating=R&lang=en"
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-          }).then(function(response) {
-            //grab actors id, inner html will be 
-            // $(actors).html(r)
-            console.log(response)
+        // $("#select-gif").on("submit", function(){
+        $("form").submit(function() {
+            var input = $("#gif-input").val()
+            console.log(input)
 
-          })
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function(response) {
+                //grab actors id, inner html will be 
+                // $(actors).html(r)
+                //run through data array
+                var dataArr = response.data
+                for (var i = 0; i <= response.data.length; i++) {
+                    console.log(response.data)
+                    // console.log(dataArr[1])
+                    console.log(response.data[i].images.downsized.url)
+                    // console.log(response.data.images)
+                    var gifUrl = response.data[i].images.downsized.url
+                    var gifs = $(".gifImages")
+                    gifs.append("<img class = 'gif-images' src='" + gifUrl + "'>")
+    
+                }
+            })
+        })
+    // })
 })
