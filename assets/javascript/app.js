fb5667873903d9368e52c051efd361c38689ea38
Laura Salcido <idlaurasalcido@gmail.com>
2018-02-21 17:18:35 -0800

    finished app, all buttons work, can go back for css but not necessary

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,18 +3,44 @@ $(document).ready(function(){
     var categories = [
         'coding', 'robots', 'anime'
     ]
+
     var gifThumb;
 
     // create for loop to create buttons and append
     for (var i = 0; i <categories.length; i++) {
         var setBtns = $("<button class='gifBtn' data-name='" + categories[i] + "'>" + categories[i] + "</button>")
         $(".gifBtns").append(setBtns)
+        
+        $(".gifBtn").on("click", function(){
+            var input = $(this).attr("data-name")
+            var APIKey = "y5KIXr4zxOUPpyUsmrtlZQejlJGkK563"
+            var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIKey + "&q=" + input + "&limit=12&offset=0&rating=R&lang=en"
 
-        setBtns.on("click", function(){
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            //run through data array
+            var dataArr = response.data
+            for (var i = 0; i <= dataArr.length; i++) {
+                var gifImage = dataArr[i].images.original_still.url
+                var gifUrl = dataArr[i].images.original.url
+                var gifRating = dataArr[i].rating
+                var gifs = $(".gifImages")
+                gifThumb = $("<img class='gif-ind' src='" + gifImage + "' data-hover='" + gifUrl + "' data-off='" + gifImage + "'><div class='bottom-left'> Rated '" + gifRating + "'</div>")
+                gifs.prepend(gifThumb)     
+                gifThumb.mouseover(function(){
+                    $(this).attr('src', $(this).data("hover"))
+                }).mouseout(function(){
+                    $(this).attr('src', $(this).data("off"))
+                })
+            }
+        })
             console.log(this)
             event.preventDefault()
         })
     }
+
 
     //click function for inputting new categories
     $("#select-gif").on("click", function(){
@@ -37,9 +63,35 @@ $(document).ready(function(){
             for (var i = 0; i <= dataArr.length; i++) {
                 var gifImage = dataArr[i].images.original_still.url
                 var gifUrl = dataArr[i].images.original.url
+                var gifRating = dataArr[i].rating
                 var gifs = $(".gifImages")
-                gifThumb = $("<img class='gif-ind' src='" + gifImage + "' data-hover='" + gifUrl + "' data-off='" + gifImage + "'>")
-                var rating = dataArr[i].rating
+                gifThumb = $("<img class='gif-ind' src='" + gifImage + "' data-hover='" + gifUrl + "' data-off='" + gifImage + "'><div class='bottom-left'> Rated '" + gifRating + "'</div>")
+                gifs.prepend(gifThumb)     
+                gifThumb.mouseover(function(){
+                    $(this).attr('src', $(this).data("hover"))
+                }).mouseout(function(){
+                    $(this).attr('src', $(this).data("off"))
+                })
+            }
+        })
+        event.preventDefault()
+        $(".gifBtn").on("click", function(){
+            var input = $(this).attr("data-name")
+            var APIKey = "y5KIXr4zxOUPpyUsmrtlZQejlJGkK563"
+            var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIKey + "&q=" + input + "&limit=12&offset=0&rating=R&lang=en"
+            
+            $.ajax({
+                url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            //run through data array
+            var dataArr = response.data
+            for (var i = 0; i <= dataArr.length; i++) {
+                var gifImage = dataArr[i].images.original_still.url
+                var gifUrl = dataArr[i].images.original.url
+                var gifRating = dataArr[i].rating
+                var gifs = $(".gifImages")
+                gifThumb = $("<img class='gif-ind' src='" + gifImage + "' data-hover='" + gifUrl + "' data-off='" + gifImage + "'><div class='bottom-left'> Rated '" + gifRating + "'</div>")
                 gifs.prepend(gifThumb)     
                 gifThumb.mouseover(function(){
                     $(this).attr('src', $(this).data("hover"))
@@ -50,4 +102,5 @@ $(document).ready(function(){
         })
         event.preventDefault()
     })
+})
 })
--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -1,50 +1,46 @@
 $(document).ready(function(){
-    // categories for preset buttons 
-    var categories = [
-        'coding', 'robots', 'anime'
-    ]
 
-    // create for loop to create buttons and append
-    for (var i = 0; i <categories.length; i++) {
-        var setBtns = $("<button class='gifBtn' value='" + categories[i] + "' data-name='" + categories[i] + "'>" + categories[i] + "</button>")
-        $(".gifBtns").append(setBtns)
-    }
-    
-    //click function for inputting new categories
+    //click function for gifting new categories
     $("#select-gif").on("click", function(){
-        //value of whatever user input
+        //value of whatever user gif
         var input = $("#gif-input").val()
-        categories.push(input)
-        console.log(categories)
-        //add new button 
-        // var newBtn = $("<button class='gifBtn' value='" + input + "' data-name='" + input + "'>" + input + "</button>")
-        // $(".gifBtns").append(newBtn)
-
-        //my unique API key
-        var APIKey = "y5KIXr4zxOUPpyUsmrtlZQejlJGkK563"
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIKey + "&q=" + input + "&limit=12&offset=0&rating=R&lang=en"
-
+        $(".gifBtns").append($("<button class='gifBTN' data-name='" + input +"'>" + input + "</button>"))
         event.preventDefault()
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-            //run through data array
-            var dataArr = response.data
-            for (var i = 0; i <= dataArr.length; i++) {
-                var gifImage = dataArr[i].images.original_still.url
-                var gifUrl = dataArr[i].images.original.url
+        
+        
+        $(".gifBTN").on("click", function (){
+            var input = $(this).attr("data-name")
+            var APIKey = "y5KIXr4zxOUPpyUsmrtlZQejlJGkK563"
+            var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIKey + "&q=" + input + "&limit=12&offset=0&rating=R&lang=en"
+            
+            console.log(queryURL)
+            event.preventDefault()
+            
+            $.ajax({
+                url: queryURL,
+                method: "GET",
+            }).then(function(r) {
+                
+                console.log(r)
+                
+                var dataArr = r.data
                 var gifs = $(".gifImages")
-                var gifThumb = $("<img class='gif-ind' value='" + i + "' src='" + gifImage + "' data-hover='" + gifUrl + "' data-off='" + gifImage + "'>")
-                // var rating = dataArr[i].rating
-                gifs.prepend(gifThumb)     
-                gifThumb.mouseover(function(){
-                    $(this).attr('src', $(this).data("hover"))
-                }).mouseout(function(){
-                    $(this).attr('src', $(this).data("off"))
-                })
-            }
+                for (var i = 0; i <= dataArr.length; i++) {
+                    var gifUrl = dataArr[i].images.original.url
+                    var gifRating = dataArr[i].rating
+                    var gifImage = dataArr[i].images.original_still.url
+                    gifThumb = $("<img class='gif-ind' src='" + gifImage + "' data-hover='" + gifUrl + "' data-off='" + gifImage + "'>")
+                    gifs.prepend(gifThumb)     
+                    gifThumb.mouseover(function(){
+                        $(this).attr('src', $(this).data("hover"))
+                    }).mouseout(function(){
+                        $(this).attr('src', $(this).data("off"))
+                    })
+                }
+                event.preventDefault()
+
+            })
+
         })
+    }) 
     })
-})
